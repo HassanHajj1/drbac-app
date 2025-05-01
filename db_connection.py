f4404884d44184b3143326735db07e44a448c1e9
Hassan <hasanhajj21@icloud.com>
2025-05-01 21:49:19 +0300

    Update DB connection to use Render PostgreSQL

--- db_connection.py
+++ db_connection.py
@@ -1,11 +1,4 @@
 import psycopg2
 import os
 def get_db_connection():
-   conn = psycopg2.connect(
-       dbname='drbac_db',
-       user='drbac_db_user',
-       password='riTsJExkMR0QBrb9P9fK6R5nInSi1HcQ',
-       host='dpg-d09q58ili9vc73ao3le0-a.oregon-postgres.render.com',
-       port='5432'
-   )
-   return conn
+   return psycopg2.connect(os.environ['DATABASE_URL'])
