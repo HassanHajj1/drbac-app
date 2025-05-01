5fb8674accb44cddd8f688b5ae263e02b3a25a6e
Hassan <hasanhajj21@icloud.com>
2025-05-01 21:38:07 +0300

    Connect to Render DB

--- db_connection.py
+++ db_connection.py
@@ -1,5 +1,11 @@
 import psycopg2
 import os
 def get_db_connection():
-   conn = psycopg2.connect(os.environ.get('DATABASE_URL'))
+   conn = psycopg2.connect(
+       dbname='drbac_db',
+       user='drbac_db_user',
+       password='riTsJExkMR0QBrb9P9fK6R5nInSi1HcQ',
+       host='dpg-d09q58ili9vc73ao3le0-a.oregon-postgres.render.com',
+       port='5432'
+   )
    return conn
